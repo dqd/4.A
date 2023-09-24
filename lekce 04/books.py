3f74378889cfa21e01b32c6191236915450e9e17
Pavel Mises <id@dqd.cz>
2023-09-24 12:16:48 +0200

    improved books.py, a new CSV dataset

--- lekce 04/books.py
+++ lekce 04/books.py
@@ -1,8 +1,12 @@
 import json
+from pathlib import Path
 
 # Zjištění průměrného věku knih v souboru books.json.
 
-with open("books.json", "r") as f:
+# Soubor je umístěn ve stejném adresáři
+path = Path(__file__).parent / Path("books.json")
+
+with path.open("r") as f:
     content = json.load(f)
 
 books = content.get("books", [])
@@ -13,4 +17,5 @@ for book in books:
     if year := book["year_published"]:
         years.append(year)
 
+# Výpočet průměrného roku vydání
 print(sum(years) // len(years))
