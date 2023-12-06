13cfc1468dfbf7318426e46858060b93b850e052
Pavel Mises <id@dqd.cz>
2023-12-06 17:12:05 +0100

    registration form

--- lekce 09/myproject/myapp/urls.py
+++ lekce 09/myproject/myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/registration/", views.registration, name="registration"),
 ]
--- lekce 09/myproject/myapp/views.py
+++ lekce 09/myproject/myapp/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def registration(request):
+    if request.user.is_authenticated:
+        raise PermissionDenied
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/registration.html", {"form": form})
